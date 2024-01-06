c20f44ac59ce14c93ccec9a1af0bb053ac305ffc
Nihalroshan55 <82870227+Nihalroshan55@users.noreply.github.com>
2024-01-06 11:33:18 +0000

    added login

--- Home/views.py
+++ Home/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, status
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-
+from rest_framework.decorators import action
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -42,6 +42,35 @@ class ClubViewSet(viewsets.ModelViewSet):
             'access': str(refresh.access_token),
         }
         return Response(data, status=status.HTTP_201_CREATED)
+    
+    @action(detail=False, methods=['POST'])        
+    def login(self, request):
+        email = request.data.get('email')
+        password=request.data.get('password')
+        if email :
+            try:
+                club=Club.objects.get(email=email)
+                if not club.is_active:
+                            return Response({"message": "Your account has been deactivated by admin"},status=status.HTTP_403_FORBIDDEN)
+                if club.check_password(password):
+                    refresh = RefreshToken.for_user(club)
+                    data = {
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token),
+                        }
+                    return Response(data, status=status.HTTP_200_OK)
+            except:
+                    return Response({"message":"Invalid Credentials"},status=status.HTTP_401_UNAUTHORIZED)
+
+
+        # if request.user._is_superuser:
+            # queryset=Order.objects.filter(status='pending')
+        # else:
+        # order=Order.objects.get(order_id=order_id)
+        # order.current_position=request.user.staff.hub
+        # order.save() 
+        # # queryset=Order.objects.filter(booking__from_hub=request.user.staff.hub,status='pending')
+        # return OrderSerializer(order).data
 
 class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
