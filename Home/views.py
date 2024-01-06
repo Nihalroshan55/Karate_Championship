33a033f10c94f0a0de344e9d4ad2b867776f87a4
Hanan <hanan.vc@gmail.com>
2024-01-06 10:21:15 +0530

    Requirements.txt

--- Home/views.py
+++ Home/views.py
@@ -39,45 +39,4 @@ class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
 
-    # def update(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     serializer = self.get_serializer(instance, data=request.data, partial=True)
-    #     serializer.is_valid(raise_exception=True)
 
-    #     # Extract the values of kumite and kata from the request data
-    #     kumite = serializer.validated_data.get('kumite', instance.kumite)
-    #     kata = serializer.validated_data.get('kata', instance.kata)
-
-    #     old_entry_fee = instance.entry_fee
-
-    #     # Recalculate entry fee based on kumite and kata
-    #     entry_fee = self.calculate_entry_fee(kumite, kata)
-        
-    #     instance.club.fees -=old_entry_fee
-    #     instance.club.fees +=entry_fee
-    #     # Update the instance and save
-    #     serializer.save(entry_fee=entry_fee)
-    #     instance.club.save()
-
-        
-    #     # Update the corresponding Club's fees
-    #     # if instance.club:
-    #     #     difference = entry_fee - old_entry_fee
-    #     #     if difference:
-    #     #         print(difference,"inside > 0")
-                
-    #     #         instance.club.fees += difference
-
-    #         # elif difference < 0:
-    #         #     print(difference,"inside < 0")
-
-    #         #     instance.club.fees += difference
-                
-
-    #     return Response(serializer.data)
-
-    # def calculate_entry_fee(self, kumite, kata):
-    #     if (kata and not kumite) or (kumite and not kata):
-    #         return 1000
-    #     else:
-    #         return 1500
